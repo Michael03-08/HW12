f9b66a3a7d3f4ab62821f9768367aa27458dfa48
Flex <cma491628@gmail.com>
2023-11-14 19:25:42 +0200

    инкапсуляция, set and get

--- Task 1/src/package1/Test2.java
+++ Task 1/src/package1/Test2.java
@@ -3,5 +3,8 @@ package package1;
 public class Test2 {
     public static void main(String[] args) {
         Test1 test1 = new Test1();
+        test1.a = 5;
+        test1.b = 6;
+        test1.c = 7;
     }
 }
---
+++ Task 1/src/person/Person.java
@@ -0,0 +1,50 @@
+package person;
+
+public class Person {
+
+    private String name;
+    private String jobTitle;
+    private double age;
+    private  String placeOfWork;
+    private double salary;
+
+    private Person(){
+    }
+    private Person(String name, String jobTitle, String placeOfWork, double age, double salary){
+        this.name = name;
+        this.age = age;
+        this.jobTitle = jobTitle;
+        this.placeOfWork = placeOfWork;
+        this.salary = salary;
+    }
+    public String getName(){
+        return name;
+    }
+    public  String getJobTitle(){
+        return jobTitle;
+    }
+    public String getPlaceOfWork(){
+        return placeOfWork;
+    }
+    public double getAge(){
+        return age;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+    public void setPlaceOfWork(String placeOfWork){
+        this.placeOfWork = placeOfWork;
+    }
+    public void setAge(double age){
+        this.age = age;
+    }
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+}
